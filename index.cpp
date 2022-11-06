fc2aa1545ad5717e06950abb440b224a13aaa1cb
Michal Mordarski <michal.m1234@interia.pl>
2022-11-06 11:56:40 +0100

    ax + bx + c = 0

--- index.cpp
+++ index.cpp
@@ -3,20 +3,22 @@
 using namespace std;
 
 int main() {
-    int a,b;
+    int a,b,c;
     cout<<"Wpisz wspolczynnik a = ";
     cin>>a;
     cout<<"Wpisz wspolczynnik b = ";
     cin>>b;
+    cout<<"Wpisz wspolczynnik c = ";
+    cin>>c;
     
-    cout<<"Twoja funkcja liniowa to "<<a<<"x "<<"+ "<<b<<"\n";
+    cout<<"Twoja funkcja liniowa to "<<a<<"x "<<"+ "<<b<<"y "<<"+ "<<c<<" = 0"<<"\n";
     
-    if(a==0){
+    cout<<"Miejsce zerowe to "<<-c/a;
+    /* if(a==0){
     	cout<<"Miejsce zerowe to "<<b;
 	} else {
 		cout<<"Miejsce zerowe to "<<-b/a;
 	}
-    
-    
-    
+	*/
+
 }
