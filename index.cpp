2b86cc262a5575555a305ce314f399918071f734
Michal Mordarski <michal.m1234@interia.pl>
2022-11-06 11:47:18 +0100

    Dodanie mozliwosci a = 0

--- index.cpp
+++ index.cpp
@@ -8,7 +8,15 @@ int main() {
     cin>>a;
     cout<<"Wpisz wspolczynnik b = ";
     cin>>b;
+    
     cout<<"Twoja funkcja liniowa to "<<a<<"x "<<"+ "<<b<<"\n";
-    cout<<"Miejsce zerowe to "<<-b/a;
-    //komentarz
+    
+    if(a==0){
+    	cout<<"Miejsce zerowe to "<<b;
+	} else {
+		cout<<"Miejsce zerowe to "<<-b/a;
+	}
+    
+    
+    
 }
